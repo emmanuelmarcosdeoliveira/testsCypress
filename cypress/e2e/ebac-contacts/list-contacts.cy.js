aca6997aa2c02f492d5fbfbb076021dfe7e83789
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-08-21 12:24:51 -0300

    chore:  the test was written to test the functionality of adding contacts

--- cypress/e2e/ebac-contacts/list-contacts.cy.js
+++ cypress/e2e/ebac-contacts/list-contacts.cy.js
@@ -1,8 +1,21 @@
-<reference types="cypress" />;
+/// <reference types="cypress" />;
 
-describe("Descriçào dessa Lista de Testes", () => {
-  beforeEach();
-  it("Descrição do Teste", () => {
-    cy.get();
+describe("Deve Castratrar um contato", () => {
+  it("Deve Recarregar o navegador", () => {
+    expect(true).to.equal(true);
+  });
+  beforeEach(() => {
+    cy.visit("https://agenda-contatos-react.vercel.app/");
+  });
+  it("Deve Preencher os campos de cadastro do Formulário", () => {
+    cy.get('input[type="text"]').type("Emmanuel{enter}");
+    cy.get('input[type="email"]').type("emmanuel@gmail.com{enter}");
+    cy.get('input[type="tel"]').type("(11)-99999-9999{enter}");
+  });
+  it("Deve Ter 3 inputs preenchidos", () => {
+    cy.get("input").should("have.length", 3);
+  });
+  it("Deve fazer o click no botao adicionar", () => {
+    cy.get(".adicionar").click();
   });
 });
